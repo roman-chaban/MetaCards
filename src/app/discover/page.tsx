db5a6411fcf98076ab73521a74914d62e1deb123
Roman Chaban <roman.chaban1001@gmail.com>
2024-07-01 01:57:08 +0300

    Added Discover page, and fixed styles bugs by Header component .

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import styles from './Discover.module.scss';
 import { Cards } from '@/components/Cards/Cards';
+import { DiscoverNav } from '@/components/DiscoverNav/DiscoverNav';
+import { buttonLabels } from '@/constants/navButtonLabels';
 
 
 
@@ -16,6 +18,7 @@ export default function Discover() {
   return (
     <section className={styles.discover}>
       <div className={styles.discover__container}>
+        <DiscoverNav buttonLabels={buttonLabels} title='Discover NFTs' />
         <Cards />
       </div>
     </section>
---
+++ src/constants/navButtonLabels.tsx
@@ -0,0 +1,13 @@
+
+
+interface ButtonLabels {
+   primary: string;
+   secondary: string;
+   thirdary: string;
+}
+
+export const buttonLabels: ButtonLabels = {
+   primary: 'Category',
+   secondary: 'Collection',
+   thirdary: 'Price',
+};
