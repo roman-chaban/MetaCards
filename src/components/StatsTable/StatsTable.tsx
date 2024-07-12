22a9bc7bff28abcc585ddb8cbeec5ec5b66223ae
Roman Chaban <roman.chaban1001@gmail.com>
2024-07-12 02:34:34 -0800

    Corrected Stats page .

--- src/components/StatsTable/StatsTable.tsx
+++ src/components/StatsTable/StatsTable.tsx
@@ -5,36 +5,38 @@ import Image from 'next/image';
 
 export const StatsTable: FC = () => {
   return (
-    <table className={styles.statsTable}>
-      <tbody className={styles.stats__table_body}>
-        {statsTableItems.map((tableItem, index) => (
-          <tr key={index} className={styles.stats__table_bodyRow}>
-            <td className={styles.stats__table_dataCell}>
-              <Image
-                priority
-                src={tableItem.image}
-                alt={tableItem.title}
-                width={85}
-                height={85}
-                className={styles.table__icon}
-              />
-              <div className={styles.stats__table_dataTitles}>
-                <div className={styles.table__first_title}>
-                  {tableItem.title}
+    <div className={styles.tableContainer}>
+      <table className={styles.statsTable}>
+        <tbody className={styles.tableBody}>
+          {statsTableItems.map((tableItem, index) => (
+            <tr key={index} className={styles.statsTableRow}>
+              <td className={styles.statsTableDataCell}>
+                <Image
+                  priority
+                  src={tableItem.image}
+                  alt={tableItem.title}
+                  width={85}
+                  height={85}
+                  className={styles.tableIcon}
+                />
+                <div className={styles.statsTableDataTitles}>
+                  <div className={styles.tableFirstTitle}>
+                    {tableItem.title}
+                  </div>
+                  <div className={styles.tableSecondTitle}>
+                    {tableItem.subTitle}
+                  </div>
                 </div>
-                <div className={styles.table__second_title}>
-                  {tableItem.subTitle}
-                </div>
-              </div>
-            </td>
-            <td className={styles.volume}>{tableItem.title}</td>
-            <td className={styles.percent}>{tableItem.percent}</td>
-            <td className={styles.rating}>{tableItem.rating}</td>
-            <td className={styles.owners}>{tableItem.owner}</td>
-            <td className={styles.items}>{tableItem.items}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+              </td>
+              <td className={styles.volume}>{tableItem.title}</td>
+              <td className={styles.percent}>{tableItem.percent}</td>
+              <td className={styles.rating}>{tableItem.rating}</td>
+              <td className={styles.owners}>{tableItem.owner}</td>
+              <td className={styles.items}>{tableItem.items}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
